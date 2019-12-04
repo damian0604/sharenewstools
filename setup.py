0ede7cd831028f55eaed3ac515891d3d10d5084e
damian0604 <d.c.trilling@uva.nl>
2019-12-04 19:28:45 +0100

    make work as module

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
-#setup(
-#...
-#    scripts=['bin/url2crowdtangle'],
-#_
+setup(
+       name='sharenewstools',
+       version='0.1',
+       description='Toolkit for analyzing news sharing on social media',
+       author='Damian Trilling',
+       author_email='d.c.trilling@uva.nl',
+       packages=['sharenewstools'],  
+       install_requires=['requests', 'pandas'],  # TODO: check
+       scripts=['bin/urls2crowdtangle']
+    )
